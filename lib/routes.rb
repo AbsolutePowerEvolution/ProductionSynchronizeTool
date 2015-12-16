da96b2142e57f61f993cdad6b36313765d5acbeb
DanSnow <dododavid006@gmail.com>
2015-12-16 14:48:35 +0800

    Add homepage; Add expection handle

--- lib/routes.rb
+++ lib/routes.rb
@@ -6,17 +6,40 @@ require 'json'
 require 'sinatra'
 require 'sinatra/contrib'
 
+require_relative 'config'
+
+configure do
+  set :server, :puma
+  set :public_folder, File.join(APP_ROOT, 'public')
+end
+
+configure :production do
+  enable :reloader
+end
+
 def pull
   # TODO: Implement git pull
+end
+
+get '/' do
+  send_file File.expand_path('index.html', settings.public_folder)
 end
 
 post '/' do
   request.body.rewind
   event = request.env['HTTP_X_GITHUB_EVENT']
-  payload = JSON.parse(request.body.read, symbolize_names: true)
-  if event == 'push'
-    if payload[:ref].end_with? 'master'
-      pull
+  begin
+    payload = JSON.parse(request.body.read, symbolize_names: true)
+    if event == 'push'
+      if payload.fetch(:ref).end_with? 'master'
+        pull
+      end
+      status 200
+      body ''
+    else
+      status 422
     end
+  rescue JSON::ParserError, KeyError
+    status 422
   end
 end
