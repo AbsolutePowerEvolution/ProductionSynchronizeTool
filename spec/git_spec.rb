f9a458bd97d2d25f0b72259d3bf024dd9fe3ea04
DanSnow <dododavid006@gmail.com>
2016-01-01 12:57:33 +0800

    Add git spec

---
+++ spec/git_spec.rb
@@ -0,0 +1,38 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+require 'spec_helper'
+
+require 'git'
+
+# For mocking
+require 'rugged'
+
+describe Git do
+  let(:mock_repo) { mock }
+
+  before(:each) do
+    Rugged::Repository.stubs(:new).with('.').returns(mock_repo)
+  end
+
+  describe '#repo_modify' do
+    context 'when nothing modify' do
+      it 'should return empty array' do
+        mock_repo = mock
+        mock_repo.expects(:status).yields('foo', [:worktree_new])
+        Rugged::Repository.stubs(:new).with('.').returns(mock_repo)
+        git = Git.new
+        expect(git.repo_modify).to eq []
+      end
+    end
+
+    context 'when worktree have modify' do
+      it 'should return it filename' do
+        mock_repo.expects(:status).yields('foo', [:worktree_modified])
+          .yields('bar', [:worktree_new])
+        git = Git.new
+        expect(git.repo_modify).to eq ['foo']
+      end
+    end
+  end
+end
