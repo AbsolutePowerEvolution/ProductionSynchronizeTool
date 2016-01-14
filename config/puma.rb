00168a706da36bc770684467a3629dc4c4728768
DanSnow <dododavid006@gmail.com>
2016-01-14 12:27:00 +0800

    Daemonize puma

--- config/puma.rb
+++ config/puma.rb
@@ -2,7 +2,22 @@
 
 require_relative '../lib/config.rb'
 
-if Config.ssl
+PID_PATH = File.join ::APP_ROOT, 'tmp', 'run', 'puma.pid'
+STATE_PATH = File.join ::APP_ROOT, 'tmp', 'run', 'puma.state'
+LOG_PATH = File.join ::APP_ROOT, 'tmp', 'log'
+ACCESS_LOG = File.join LOG_PATH, 'access.log'
+ERROR_LOG = File.join LOG_PATH, 'error.log'
+
+tag 'ProductionSync'
+
+pidfile PID_PATH
+state_path STATE_PATH
+
+daemonize true
+
+stdout_redirect ACCESS_LOG, ERROR_LOG, true
+
+if Config.ssl == 'true'
   bind "ssl://#{Config.address}:#{Config.port}"\
     "?key=#{Config.key_path}&cert=#{Config.cert_path}"
 else
